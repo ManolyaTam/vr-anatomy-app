871264e88799078d0a288f42eab81f09a99b934b
AsiaShalaldeh <arwwd2002@gmail.com>
2024-02-15 12:20:39 +0300

    Add a chest and some labels and remove the red line

---
+++ vr-anatomy-app/Assets/Scripts/ObjectLabel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLabel : MonoBehaviour
+{
+    //public GameObject labelPrefab;
+    //private GameObject labelInstance;
+
+    //void Start()
+    //{
+    //    // Instantiate the label prefab
+    //    labelInstance = Instantiate(labelPrefab, transform);
+    //    // Position the label above the object
+    //    //labelInstance.transform.localPosition = new Vector3(0f, 1.5f, 0f);
+    //    // Make the label face the camera
+    //    labelInstance.transform.LookAt(Camera.main.transform);
+    //}
+
+    //void Update()
+    //{
+    //    // Hide the label if it's too far away from the player
+    //    float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+    //    if (distance > 10f)
+    //    {
+    //        labelInstance.SetActive(false);
+    //    }
+    //    else
+    //    {
+    //        labelInstance.SetActive(true);
+    //    }
+    //}
+}
